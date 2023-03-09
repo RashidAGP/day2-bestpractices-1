aa685dd13668c53a6ad41f089733285b9c23d15b
RashidAGP <agprashid@gmail.com>
2023-03-09 12:54:59 +0100

    The dice game has been debugged

--- buggy/dicegame/runner.py
+++ buggy/dicegame/runner.py
@@ -15,7 +15,7 @@ class GameRunner:
     def answer(self):
         total = 0
         for die in self.dice:
-            total += 1
+            total += die.value
         return total
 
     @classmethod
@@ -23,6 +23,8 @@ class GameRunner:
         # Probably counts wins or something.
         # Great variable name, 10/10.
         c = 0
+        wins_r = 0
+        losses_r = 0
         while True:
             runner = cls()
 
@@ -37,14 +39,16 @@ class GameRunner:
             if guess == runner.answer():
                 print("Congrats, you can add like a 5 year old...")
                 runner.wins += 1
+                wins_r +=1
                 c += 1
             else:
                 print("Sorry that's wrong")
                 print("The answer is: {}".format(runner.answer()))
                 print("Like seriously, how could you mess that up")
                 runner.loses += 1
+                losses_r += 1
                 c = 0
-            print("Wins: {} Loses {}".format(runner.wins, runner.loses))
+            print("Wins: {} Loses {}".format(wins_r,losses_r))
             runner.round += 1
 
             if c == 6:
